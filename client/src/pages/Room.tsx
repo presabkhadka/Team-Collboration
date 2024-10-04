7e3a0773a176e5fe5b2e8b3726cc9db8a927dfa2
Presab Khadka <presabkhadka30@gmail.com>
2024-10-04 11:22:46 +0545

    fix styling

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -40,46 +40,51 @@ export const Room = () => {
   const { [screenSharingId]: sharing, ...peersToShow } = peers;
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="bg-blue-500 p-4 text-white flex justify-between">
+    <div className="h-screen">
+      <div className="bg-blue-500 p-4 text-white sticky top-0  overflow-hidden">
         <div>Room id :- {id}</div>
-        <div>theme</div>
       </div>
-      <div className="flex grow">
-        {screenSharingVideo && (
-          <div className="w-4/5 pr-4">
-            <VideoPlayer stream={screenSharingVideo} />
+      <div className="grid grid-cols-12 h-full ">
+        <div className="col-span-9 h-full flex flex-col">
+          <div className="flex grow h-full">
+            {screenSharingVideo && (
+              <div className="w-4/5 pr-4">
+                <VideoPlayer stream={screenSharingVideo} />
+              </div>
+            )}
+            <div
+              className={`grid  gap-4 ${
+                screenSharingVideo ? "w-1/5 grid-cols-1" : "grid-cols-4"
+              }`}
+            >
+              {screenSharingId !== me?.id && (
+                <div>
+                  <VideoPlayer stream={stream} />
+                  {/* name change garne bhaye */}
+                  {/* <NameInput /> */}
+                </div>
+              )}
+
+              {Object.values(peersToShow as PeerState)
+                .filter((peer) => !!peer.stream)
+                .map((peer) => (
+                  <div>
+                    <VideoPlayer stream={peer.stream} />
+                    <div>{peer.userName}</div>
+                  </div>
+                ))}
+            </div>
           </div>
-        )}
-        <div
-          className={`grid  gap-4 ${
-            screenSharingVideo ? "w-1/5 grid-cols-1" : "grid-cols-4"
-          }`}
-        >
-          {screenSharingId !== me?.id && (
-            <div>
-              <VideoPlayer stream={stream} />
-              {/* name change garne bhaye */}
-              {/* <NameInput /> */}
+        </div>
+        <div className="col-span-3 h-full">
+          {chat.isChatOpen && (
+            <div className="border-l-2 pb-24 h-full ">
+              <Chat />
             </div>
           )}
-
-          {Object.values(peersToShow as PeerState)
-            .filter((peer) => !!peer.stream)
-            .map((peer) => (
-              <div>
-                <VideoPlayer stream={peer.stream} />
-                <div>{peer.userName}</div>
-              </div>
-            ))}
         </div>
-        {chat.isChatOpen && (
-          <div className="border-l-2 pb-24 ">
-            <Chat />
-          </div>
-        )}
       </div>
-      <div className="h-24 fixed bottom-0 p-4 w-full flex justify-center items-center border-t-2 bg-white gap-2">
+      <div className="h-24 fixed bottom-0 p-4 w-full flex justify-center items-center border-t-2 bg-white gap-2 ">
         <ShareScreenButton onClick={shareScreen} />
         <ChatButton onClick={toggleChat} />
       </div>
