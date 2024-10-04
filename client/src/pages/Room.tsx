ec35470aae27ad48b38951ec0de231d75ce342af
presabkhadkabrahmabyte <brahmabytelab03@gmail.com>
2024-10-04 12:54:52 +0545

    updt sheet on chat btn

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -1,4 +1,4 @@
- import { useContext, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { RoomContext } from "../context/RoomContext";
 import { VideoPlayer } from "../components/VideoPlayer";
@@ -6,7 +6,6 @@ import { ShareScreenButton } from "../components/ShareScreenButton";
 import { ChatButton } from "../components/ChatButton";
 import { Chat } from "../components/chat/Chat";
 import { PeerState } from "../reducers/peerReducer";
-import { NameInput } from "../common/Name";
 import { CanvasButton } from "../components/CanvasButton";
 
 export const Room = () => {
@@ -46,7 +45,7 @@ export const Room = () => {
         <div>Room id :- {id}</div>
       </div>
       <div className="grid grid-cols-12 h-full ">
-        <div className="col-span-9 h-full flex flex-col">
+        <div className="col-span-full h-full flex flex-col">
           <div className="flex grow h-full">
             {screenSharingVideo && (
               <div className="w-4/5 pr-4">
@@ -61,8 +60,8 @@ export const Room = () => {
               {screenSharingId !== me?.id && (
                 <div>
                   <VideoPlayer stream={stream} />
-                  {/* name change garne bhaye */}
-                  {/* <NameInput /> */}
+                  {/* name change garne bhaye
+                  <NameInput /> */}
                 </div>
               )}
 
@@ -77,18 +76,18 @@ export const Room = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-3 h-full">
+        {/* <div className="col-span-3 h-full">
           {chat.isChatOpen && (
             <div className="border-l-2 pb-24 h-full ">
               <Chat />
             </div>
           )}
-        </div>
+        </div> */}
       </div>
       <div className="h-24 fixed bottom-0 p-4 w-full flex justify-center items-center border-t-2 bg-white gap-2 ">
         <ShareScreenButton onClick={shareScreen} />
         <ChatButton onClick={toggleChat} />
-        <CanvasButton/>
+        <CanvasButton />
       </div>
     </div>
   );
