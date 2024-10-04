966b52c100483b13756a038178fe93aa9231d66a
presabkhadkabrahmabyte <brahmabytelab03@gmail.com>
2024-10-04 14:34:10 +0545

    implemented tailwind scrollbar

--- client/src/components/ChatButton.tsx
+++ client/src/components/ChatButton.tsx
@@ -26,11 +26,11 @@ export const ChatButton: React.FC = () => {
           />
         </svg>
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent className="scrollbar scrollbar-thumb-blue-500">
         <SheetHeader>
           <SheetTitle className="mx-2">Team Chat</SheetTitle>
         </SheetHeader>
-        <div className="overflow-y-scroll h-full pb-10">
+        <div className="overflow-y-scroll h-full pb-10  scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-blue-100">
           <Chat />
         </div>
       </SheetContent>
